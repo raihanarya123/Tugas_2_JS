8a797217dfeaeef9727bfb741cfe1b8a334993c6
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:06:40 +0700

    Tugas 2 JS
    2

---
+++ tugasjs2.js
@@ -0,0 +1,10 @@
+function tugas2() {
+    let var1 = 10;
+    var2 = "Raihan arya";
+    var3 = true;
+    var4 = undefined;
+    var5 = null;
+    const var_array = [1,2,3];
+    console.log(var1 + " " + var2 + " " + var3 + " " + var4 + " " + var5 + " " + var_array);
+}
+tugas2();
